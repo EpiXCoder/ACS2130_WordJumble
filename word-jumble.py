780a3832bf42b3c128f3a2c4d3fe6ba68c72c5a3
Vithusha Ravirajan <vithusharavirajan@Vithus-MBP.local>
2023-05-12 22:38:43 -0400

    added recursion to jumble

--- word-jumble.py
+++ word-jumble.py
@@ -1,10 +1,6 @@
 # Part 1
 # Unscramble the words
 def unscramble(jumble):
-    # Read the dictionary file into a set
-    with open('words2', 'r') as f:
-        dictionary = set([word.strip().lower() for word in f])
-
     # Create a hash table for the dictionary words
     hashtable = {}
     for word in dictionary:
@@ -20,52 +16,69 @@ def unscramble(jumble):
         valid_words = hashtable[jumble_key]
     else:
         valid_words = []
-    
+
     return valid_words
 
-#pick out letters to be used in the final word using indices
-def final_letters():
+# pick out letters to be used in the final word using indices
+def final_letters(jumbles, indices):
     valid_word_lists = [unscramble(jumble) for jumble in jumbles]
-    # valid_word_lists = [['often'], ['kiosk'], ['immune'], ['cousin']]
     letters_array = []
 
     for i, word in enumerate(valid_word_lists):
         for index in indices[i]:
             letters_array.append(word[0][index])
 
-    # print(letters_array)
-    return letters_array
+    return ''.join(letters_array)
 
 # Part 2
-# Using the letters, create valid word pairs
-def create_word_pairs():
-    letters_array = final_letters()
-    word_pairs = []
-    with open('words2', 'r') as file:
-        word_list = [line.strip().lower() for line in file]
+# Using the letters and recursion, create valid word arrangements
+def final_word_combinations(letters):
+    if len(letters) == 1:
+        return [letters]
+
+    arrangements = []
+    for i in range(len(letters)):
+        current_letter = letters[i]
+        remaining_letters = letters[:i] + letters[i+1:]
+        sub_arrangements = final_word_combinations(remaining_letters)
+        for arrangement in sub_arrangements:
+            arrangements.append(current_letter + arrangement)
+
+    return arrangements
+
+
+if __name__ == "__main__":
+
+    # Read the dictionary file into a set
+    with open('words2', 'r') as f:
+        dictionary = set([word.strip().lower() for word in f])
+
+    jumbles = ['tefon', 'sokik', 'niumem', 'siconu']
+    indices = [[2, 4], [0, 1, 3], [4], [3, 4]]
+    valid_words = [unscramble(jumble) for jumble in jumbles]
+    letters = final_letters(jumbles, indices)
     
-    # Get a list of two letter words and 6 letter valid words using the letters
-    two_letter_words = [word for word in word_list if len(word) == 2]
-    six_letter_words = [word for word in word_list if len(word) == 6]
 
-    # Pair up the words
-    for word1 in two_letter_words:
-        for word2 in six_letter_words:
-            if sorted(list(word1 + word2)) == sorted(letters_array):
-                word_pairs.append((word1, word2))
+    arrangements = final_word_combinations(letters)
 
-    return word_pairs
+    arrangement_list = []
+    for arrangement in arrangements:
+        arrangement_list.append(''.join(arrangement))
+
+    # print(arrangement_list)
 
 
 
-def jumble_solution():
-    result = create_word_pairs()
+     # Get a list of two letter words and 6 letter valid words using the letters
+    possible_finals = []
+    for word in arrangement_list:
+        if word[0:2] in dictionary and word[2:] in dictionary:
+            if word not in possible_finals:
+                possible_finals.append(word)
 
-# Print the resulting word pairs
-    for pair in result:
-        print(pair)
+    print(f"Jumbles: {jumbles}")
+    print(f"Solved words: {valid_words}")
+    print(f"Final letters for solution: {letters}")
+    print(f"Possible final words: {possible_finals}")
 
-# Define the jumbles and indices
-jumbles = ['tefon', 'sokik', 'niumem', 'siconu']
-indices = [[2, 4], [0, 1, 3], [4], [3, 4]]
-jumble_solution()
+
